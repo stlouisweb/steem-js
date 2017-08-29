2c593ab76ba1ce6310416fe55aba35e0150d8e9a
stlouisweb <stlouisweb@gmail.com>
2017-08-29 10:30:58 -0500

    adding bcof profile methods - WIP.

--- src/api/transports/ws.js
+++ src/api/transports/ws.js
@@ -1,29 +1,30 @@
-import Promise from 'bluebird';
-import defaults from 'lodash/defaults';
-import isNode from 'detect-node';
-import newDebug from 'debug';
+import Promise from "bluebird";
+import defaults from "lodash/defaults";
+import isNode from "detect-node";
+import newDebug from "debug";
 
-import Transport from './base';
+import Transport from "./base";
 
 let WebSocket;
 if (isNode) {
-  WebSocket = require('ws'); // eslint-disable-line global-require
-} else if (typeof window !== 'undefined') {
+  WebSocket = require("ws"); // eslint-disable-line global-require
+} else if (typeof window !== "undefined") {
   WebSocket = window.WebSocket;
 } else {
   throw new Error("Couldn't decide on a `WebSocket` class");
 }
 
-const debug = newDebug('steem:ws');
+const debug = newDebug("steem:ws");
 
 const DEFAULTS = {
   apiIds: {
     database_api: 0,
     login_api: 1,
     follow_api: 2,
-    network_broadcast_api: 4,
+    tracker_api: 3,
+    network_broadcast_api: 4
   },
-  id: 0,
+  id: 0
 };
 
 export default class WsTransport extends Transport {
@@ -54,15 +55,15 @@ export default class WsTransport extends Transport {
       const url = this.options.websocket;
       this.ws = new WebSocket(url);
 
-      const releaseOpen = this.listenTo(this.ws, 'open', () => {
-        debug('Opened WS connection with', url);
+      const releaseOpen = this.listenTo(this.ws, "open", () => {
+        debug("Opened WS connection with", url);
         this.isOpen = true;
         releaseOpen();
         resolve();
       });
 
-      const releaseClose = this.listenTo(this.ws, 'close', () => {
-        debug('Closed WS connection with', url);
+      const releaseClose = this.listenTo(this.ws, "close", () => {
+        debug("Closed WS connection with", url);
         this.isOpen = false;
         delete this.ws;
         this.stop();
@@ -70,19 +71,19 @@ export default class WsTransport extends Transport {
         if (startP.isPending()) {
           reject(
             new Error(
-              'The WS connection was closed before this operation was made',
-            ),
+              "The WS connection was closed before this operation was made"
+            )
           );
         }
       });
 
-      const releaseMessage = this.listenTo(this.ws, 'message', message => {
-        debug('Received message', message.data);
+      const releaseMessage = this.listenTo(this.ws, "message", message => {
+        debug("Received message", message.data);
         const data = JSON.parse(message.data);
         const id = data.id;
         const request = this.requests[id];
         if (!request) {
-          debug('Steem.onMessage: unknown request ', id);
+          debug("Steem.onMessage: unknown request ", id);
         }
         delete this.requests[id];
         this.onMessage(data, request);
@@ -91,7 +92,7 @@ export default class WsTransport extends Transport {
       this.releases = this.releases.concat([
         releaseOpen,
         releaseClose,
-        releaseMessage,
+        releaseMessage
       ]);
     });
 
@@ -102,7 +103,7 @@ export default class WsTransport extends Transport {
   }
 
   stop() {
-    debug('Stopping...');
+    debug("Stopping...");
     if (this.ws) this.ws.close();
     this.apiIdsP = {};
     delete this.startP;
@@ -127,9 +128,9 @@ export default class WsTransport extends Transport {
       ? [requestName]
       : Object.keys(this.apiIds);
     apiNamesToRefresh.forEach(name => {
-      this.apiIdsP[name] = this.sendAsync('login_api', {
-        method: 'get_api_by_name',
-        params: [name],
+      this.apiIdsP[name] = this.sendAsync("login_api", {
+        method: "get_api_by_name",
+        params: [name]
       }).then(result => {
         if (result != null) {
           this.apiIds[name] = result;
@@ -147,18 +148,20 @@ export default class WsTransport extends Transport {
   }
 
   send(api, data, callback) {
-    debug('Steem::send', api, data);
+    console.log(data);
+    debug("Steem::send", api, data);
     const id = data.id || this.id++;
     const startP = this.start();
 
-    const apiIdsP = api === 'login_api' && data.method === 'get_api_by_name'
-      ? Promise.fulfilled()
-      : this.getApiIds(api);
+    const apiIdsP =
+      api === "login_api" && data.method === "get_api_by_name"
+        ? Promise.fulfilled()
+        : this.getApiIds(api);
 
-    if (api === 'login_api' && data.method === 'get_api_by_name') {
-      debug('Sending setup message');
+    if (api === "login_api" && data.method === "get_api_by_name") {
+      debug("Sending setup message");
     } else {
-      debug('Going to wait for setup messages to resolve');
+      debug("Going to wait for setup messages to resolve");
     }
 
     this.currentP = Promise.join(startP, apiIdsP)
@@ -168,30 +171,30 @@ export default class WsTransport extends Transport {
             if (!this.ws) {
               reject(
                 new Error(
-                  'The WS connection was closed while this request was pending',
-                ),
+                  "The WS connection was closed while this request was pending"
+                )
               );
               return;
             }
 
             const payload = JSON.stringify({
               id,
-              method: 'call',
-              params: [this.apiIds[api], data.method, data.params],
+              method: "call",
+              params: [this.apiIds[api], data.method, data.params]
             });
 
-            debug('Sending message', payload);
+            debug("Sending message", payload);
             this.requests[id] = {
               api,
               data,
               resolve,
               reject,
-              start_time: Date.now(),
+              start_time: Date.now()
             };
 
             // this.inFlight += 1;
             this.ws.send(payload);
-          }),
+          })
       )
       .nodeify(callback);
 
@@ -199,30 +202,30 @@ export default class WsTransport extends Transport {
   }
 
   onMessage(message, request) {
-    const {api, data, resolve, reject, start_time} = request;
-    debug('-- Steem.onMessage -->', message.id);
+    const { api, data, resolve, reject, start_time } = request;
+    debug("-- Steem.onMessage -->", message.id);
     const errorCause = message.error;
     if (errorCause) {
       const err = new Error(
         // eslint-disable-next-line prefer-template
-        (errorCause.message || 'Failed to complete operation') +
-          ' (see err.payload for the full error payload)',
+        (errorCause.message || "Failed to complete operation") +
+          " (see err.payload for the full error payload)"
       );
       err.payload = message;
       reject(err);
       return;
     }
 
-    if (api === 'login_api' && data.method === 'login') {
+    if (api === "login_api" && data.method === "login") {
       debug(
         "network_broadcast_api API ID depends on the WS' session. " +
-          'Triggering a refresh...',
+          "Triggering a refresh..."
       );
-      this.getApiIds('network_broadcast_api', true);
+      this.getApiIds("network_broadcast_api", true);
     }
 
-    debug('Resolved', api, data, '->', message);
-    this.emit('track-performance', data.method, Date.now() - start_time);
+    debug("Resolved", api, data, "->", message);
+    this.emit("track-performance", data.method, Date.now() - start_time);
     delete this.requests[message.id];
     resolve(message.result);
   }
--- src/broadcast/operations.js
+++ src/broadcast/operations.js
@@ -1,18 +1,13 @@
 module.exports = [
   {
-    "roles": ["posting"],
-    "operation": "vote",
-    "params": [
-      "voter",
-      "author",
-      "permlink",
-      "weight"
-    ]
+    roles: ["posting"],
+    operation: "vote",
+    params: ["voter", "author", "permlink", "weight"]
   },
   {
-    "roles": ["posting"],
-    "operation": "comment",
-    "params": [
+    roles: ["posting"],
+    operation: "comment",
+    params: [
       "parent_author",
       "parent_permlink",
       "author",
@@ -23,36 +18,24 @@ module.exports = [
     ]
   },
   {
-    "roles": ["active", "owner"],
-    "operation": "transfer",
-    "params": [
-      "from",
-      "to",
-      "amount",
-      "memo"
-    ]
+    roles: ["active", "owner"],
+    operation: "transfer",
+    params: ["from", "to", "amount", "memo"]
   },
   {
-    "roles": ["active"],
-    "operation": "transfer_to_vesting",
-    "params": [
-      "from",
-      "to",
-      "amount"
-    ]
+    roles: ["active"],
+    operation: "transfer_to_vesting",
+    params: ["from", "to", "amount"]
   },
   {
-    "roles": ["active"],
-    "operation": "withdraw_vesting",
-    "params": [
-      "account",
-      "vesting_shares"
-    ]
+    roles: ["active"],
+    operation: "withdraw_vesting",
+    params: ["account", "vesting_shares"]
   },
   {
-    "roles": ["active"],
-    "operation": "limit_order_create",
-    "params": [
+    roles: ["active"],
+    operation: "limit_order_create",
+    params: [
       "owner",
       "orderid",
       "amount_to_sell",
@@ -62,42 +45,29 @@ module.exports = [
     ]
   },
   {
-    "roles": ["active"],
-    "operation": "limit_order_cancel",
-    "params": [
-      "owner",
-      "orderid"
-    ]
+    roles: ["active"],
+    operation: "limit_order_cancel",
+    params: ["owner", "orderid"]
   },
   {
-    "roles": ["active"],
-    "operation": "price",
-    "params": [
-      "base",
-      "quote"
-    ]
+    roles: ["active"],
+    operation: "price",
+    params: ["base", "quote"]
   },
   {
-    "roles": ["active"],
-    "operation": "feed_publish",
-    "params": [
-      "publisher",
-      "exchange_rate"
-    ]
+    roles: ["active"],
+    operation: "feed_publish",
+    params: ["publisher", "exchange_rate"]
   },
   {
-    "roles": ["active"],
-    "operation": "convert",
-    "params": [
-      "owner",
-      "requestid",
-      "amount"
-    ]
+    roles: ["active"],
+    operation: "convert",
+    params: ["owner", "requestid", "amount"]
   },
   {
-    "roles": ["active"],
-    "operation": "account_create",
-    "params": [
+    roles: ["active"],
+    operation: "account_create",
+    params: [
       "fee",
       "creator",
       "new_account_name",
@@ -109,9 +79,14 @@ module.exports = [
     ]
   },
   {
-    "roles": ["owner", "active"],
-    "operation": "account_update",
-    "params": [
+    roles: ["active"],
+    operation: "profile_create",
+    params: ["account_options", "crop_assertion", "location", "name"]
+  },
+  {
+    roles: ["owner", "active"],
+    operation: "account_update",
+    params: [
       "account",
       "owner",
       "active",
@@ -121,74 +96,44 @@ module.exports = [
     ]
   },
   {
-    "roles": ["active"],
-    "operation": "witness_update",
-    "params": [
-      "owner",
-      "url",
-      "block_signing_key",
-      "props",
-      "fee"
-    ]
+    roles: ["active"],
+    operation: "witness_update",
+    params: ["owner", "url", "block_signing_key", "props", "fee"]
   },
   {
-    "roles": ["posting"],
-    "operation": "account_witness_vote",
-    "params": [
-      "account",
-      "witness",
-      "approve"
-    ]
+    roles: ["posting"],
+    operation: "account_witness_vote",
+    params: ["account", "witness", "approve"]
   },
   {
-    "roles": ["posting"],
-    "operation": "account_witness_proxy",
-    "params": [
-      "account",
-      "proxy"
-    ]
+    roles: ["posting"],
+    operation: "account_witness_proxy",
+    params: ["account", "proxy"]
   },
   {
-    "roles": ["active"],
-    "operation": "pow",
-    "params": [
-      "worker",
-      "input",
-      "signature",
-      "work"
-    ]
+    roles: ["active"],
+    operation: "pow",
+    params: ["worker", "input", "signature", "work"]
   },
   {
-    "roles": ["active"],
-    "operation": "custom",
-    "params": [
-      "required_auths",
-      "id",
-      "data"
-    ]
+    roles: ["active"],
+    operation: "custom",
+    params: ["required_auths", "id", "data"]
   },
   {
-    "roles": ["posting"],
-    "operation": "delete_comment",
-    "params": [
-      "author",
-      "permlink"
-    ]
+    roles: ["posting"],
+    operation: "delete_comment",
+    params: ["author", "permlink"]
   },
   {
-    "roles": ["posting", "active"],
-    "operation": "custom_json",
-    "params": [
-      "required_auths",
-      "required_posting_auths",
-      "id",
-      "json"
-    ]
+    roles: ["posting", "active"],
+    operation: "custom_json",
+    params: ["required_auths", "required_posting_auths", "id", "json"]
   },
   {
-    "roles": ["posting"],
-    "operation": "comment_options",
-    "params": [
+    roles: ["posting"],
+    operation: "comment_options",
+    params: [
       "author",
       "permlink",
       "max_accepted_payout",
@@ -199,19 +144,14 @@ module.exports = [
     ]
   },
   {
-    "roles": ["active"],
-    "operation": "set_withdraw_vesting_route",
-    "params": [
-      "from_account",
-      "to_account",
-      "percent",
-      "auto_vest"
-    ]
+    roles: ["active"],
+    operation: "set_withdraw_vesting_route",
+    params: ["from_account", "to_account", "percent", "auto_vest"]
   },
   {
-    "roles": ["active"],
-    "operation": "limit_order_create2",
-    "params": [
+    roles: ["active"],
+    operation: "limit_order_create2",
+    params: [
       "owner",
       "orderid",
       "amount_to_sell",
@@ -221,26 +161,19 @@ module.exports = [
     ]
   },
   {
-    "roles": ["posting"],
-    "operation": "challenge_authority",
-    "params": [
-      "challenger",
-      "challenged",
-      "require_owner"
-    ]
+    roles: ["posting"],
+    operation: "challenge_authority",
+    params: ["challenger", "challenged", "require_owner"]
   },
   {
-    "roles": ["active", "owner"],
-    "operation": "prove_authority",
-    "params": [
-      "challenged",
-      "require_owner"
-    ]
+    roles: ["active", "owner"],
+    operation: "prove_authority",
+    params: ["challenged", "require_owner"]
   },
   {
-    "roles": ["active"],
-    "operation": "request_account_recovery",
-    "params": [
+    roles: ["active"],
+    operation: "request_account_recovery",
+    params: [
       "recovery_account",
       "account_to_recover",
       "new_owner_authority",
@@ -248,9 +181,9 @@ module.exports = [
     ]
   },
   {
-    "roles": ["owner"],
-    "operation": "recover_account",
-    "params": [
+    roles: ["owner"],
+    operation: "recover_account",
+    params: [
       "account_to_recover",
       "new_owner_authority",
       "recent_owner_authority",
@@ -258,18 +191,14 @@ module.exports = [
     ]
   },
   {
-    "roles": ["owner"],
-    "operation": "change_recovery_account",
-    "params": [
-      "account_to_recover",
-      "new_recovery_account",
-      "extensions"
-    ]
+    roles: ["owner"],
+    operation: "change_recovery_account",
+    params: ["account_to_recover", "new_recovery_account", "extensions"]
   },
   {
-    "roles": ["active"],
-    "operation": "escrow_transfer",
-    "params": [
+    roles: ["active"],
+    operation: "escrow_transfer",
+    params: [
       "from",
       "to",
       "agent",
@@ -283,20 +212,14 @@ module.exports = [
     ]
   },
   {
-    "roles": ["active"],
-    "operation": "escrow_dispute",
-    "params": [
-      "from",
-      "to",
-      "agent",
-      "who",
-      "escrow_id"
-    ]
+    roles: ["active"],
+    operation: "escrow_dispute",
+    params: ["from", "to", "agent", "who", "escrow_id"]
   },
   {
-    "roles": ["active"],
-    "operation": "escrow_release",
-    "params": [
+    roles: ["active"],
+    operation: "escrow_release",
+    params: [
       "from",
       "to",
       "agent",
@@ -308,111 +231,64 @@ module.exports = [
     ]
   },
   {
-    "roles": ["active"],
-    "operation": "pow2",
-    "params": [
-      "input",
-      "pow_summary"
-    ]
+    roles: ["active"],
+    operation: "pow2",
+    params: ["input", "pow_summary"]
   },
   {
-    "roles": ["active"],
-    "operation": "escrow_approve",
-    "params": [
-      "from",
-      "to",
-      "agent",
-      "who",
-      "escrow_id",
-      "approve"
-    ]
+    roles: ["active"],
+    operation: "escrow_approve",
+    params: ["from", "to", "agent", "who", "escrow_id", "approve"]
   },
   {
-    "roles": ["active"],
-    "operation": "transfer_to_savings",
-    "params": [
-      "from",
-      "to",
-      "amount",
-      "memo"
-    ]
+    roles: ["active"],
+    operation: "transfer_to_savings",
+    params: ["from", "to", "amount", "memo"]
   },
   {
-    "roles": ["active"],
-    "operation": "transfer_from_savings",
-    "params": [
-      "from",
-      "request_id",
-      "to",
-      "amount",
-      "memo"
-    ]
+    roles: ["active"],
+    operation: "transfer_from_savings",
+    params: ["from", "request_id", "to", "amount", "memo"]
   },
   {
-    "roles": ["active"],
-    "operation": "cancel_transfer_from_savings",
-    "params": [
-      "from",
-      "request_id"
-    ]
+    roles: ["active"],
+    operation: "cancel_transfer_from_savings",
+    params: ["from", "request_id"]
   },
   {
-    "roles": ["posting", "active", "owner"],
-    "operation": "custom_binary",
-    "params": [
-      "id",
-      "data"
-    ]
+    roles: ["posting", "active", "owner"],
+    operation: "custom_binary",
+    params: ["id", "data"]
   },
   {
-    "roles": ["owner"],
-    "operation": "decline_voting_rights",
-    "params": [
-      "account",
-      "decline"
-    ]
+    roles: ["owner"],
+    operation: "decline_voting_rights",
+    params: ["account", "decline"]
   },
   {
-    "roles": ["active"],
-    "operation": "reset_account",
-    "params": [
-      "reset_account",
-      "account_to_reset",
-      "new_owner_authority"
-    ]
+    roles: ["active"],
+    operation: "reset_account",
+    params: ["reset_account", "account_to_reset", "new_owner_authority"]
   },
   {
-    "roles": ["owner", "posting"],
-    "operation": "set_reset_account",
-    "params": [
-      "account",
-      "current_reset_account",
-      "reset_account"
-    ]
+    roles: ["owner", "posting"],
+    operation: "set_reset_account",
+    params: ["account", "current_reset_account", "reset_account"]
   },
   {
-    "roles": ["posting"],
-    "operation": "claim_reward_balance",
-    "params": [
-      "account",
-      "reward_steem",
-      "reward_sbd",
-      "reward_vests"
-    ]
+    roles: ["posting"],
+    operation: "claim_reward_balance",
+    params: ["account", "reward_steem", "reward_sbd", "reward_vests"]
   },
   {
-    "roles": ["active"],
-    "operation": "delegate_vesting_shares",
-    "params": [
-      "delegator",
-      "delegatee",
-      "vesting_shares"
-    ]
+    roles: ["active"],
+    operation: "delegate_vesting_shares",
+    params: ["delegator", "delegatee", "vesting_shares"]
   },
   {
-    "roles": ["active"],
-    "operation": "account_create_with_delegation",
-    "params": [
+    roles: ["active"],
+    operation: "account_create_with_delegation",
+    params: [
       "fee",
       "delegation",
       "creator",
@@ -426,54 +302,34 @@ module.exports = [
     ]
   },
   {
-    "roles": ["active"],
-    "operation": "fill_convert_request",
-    "params": [
-      "owner",
-      "requestid",
-      "amount_in",
-      "amount_out"
-    ]
+    roles: ["active"],
+    operation: "fill_convert_request",
+    params: ["owner", "requestid", "amount_in", "amount_out"]
   },
   {
-    "roles": ["posting"],
-    "operation": "comment_reward",
-    "params": [
-      "author",
-      "permlink",
-      "payout"
-    ]
+    roles: ["posting"],
+    operation: "comment_reward",
+    params: ["author", "permlink", "payout"]
   },
   {
-    "roles": ["active"],
-    "operation": "liquidity_reward",
-    "params": [
-      "owner",
-      "payout"
-    ]
+    roles: ["active"],
+    operation: "liquidity_reward",
+    params: ["owner", "payout"]
   },
   {
-    "roles": ["active"],
-    "operation": "interest",
-    "params": [
-      "owner",
-      "interest"
-    ]
+    roles: ["active"],
+    operation: "interest",
+    params: ["owner", "interest"]
   },
   {
-    "roles": ["active"],
-    "operation": "fill_vesting_withdraw",
-    "params": [
-      "from_account",
-      "to_account",
-      "withdrawn",
-      "deposited"
-    ]
+    roles: ["active"],
+    operation: "fill_vesting_withdraw",
+    params: ["from_account", "to_account", "withdrawn", "deposited"]
   },
   {
-    "roles": ["posting"],
-    "operation": "fill_order",
-    "params": [
+    roles: ["posting"],
+    operation: "fill_order",
+    params: [
       "current_owner",
       "current_orderid",
       "current_pays",
@@ -483,14 +339,8 @@ module.exports = [
     ]
   },
   {
-    "roles": ["posting"],
-    "operation": "fill_transfer_from_savings",
-    "params": [
-      "from",
-      "to",
-      "amount",
-      "request_id",
-      "memo"
-    ]
+    roles: ["posting"],
+    operation: "fill_transfer_from_savings",
+    params: ["from", "to", "amount", "request_id", "memo"]
   }
 ];
